9c384e7fe78004db219c80d18ef76301a4cf60ad
ardikaveh <ardikaveh@gmail.com>
2015-07-25 00:28:38 -0700

    adding everything

---
+++ public/js/application.js
@@ -0,0 +1,195 @@
+$(function() {
+	// generate unique user id
+	var userId = Math.random().toString(16).substring(2,15);
+	var socket = io.connect('/', {transports: ['websocket']});
+	var map;
+
+	var info = $('#infobox');
+	var doc = $(document);
+	var chatinput = $('#chatinput');
+	var snd = new Audio("blop.wav"); // buffers automatically when created
+	
+	$('#chatinput').focus();
+	$("#chatoutput").append("<br/></br><br/><br/><br/></br><br/>");
+
+
+	$('#chatinput').bind('touchmove', function(e){
+		e.preventDefault()	
+	})
+	
+	$('#chatoutput').bind('touchend', function(e){
+		window.scrollTo(0, 0);
+	})
+
+	// custom marker's icon styles
+	var tinyIcon = L.Icon.extend({
+		options: {
+			shadowUrl: '../assets/marker-shadow.png',
+			iconSize: [25, 39],
+			iconAnchor:   [12, 36],
+			shadowSize: [41, 41],
+			shadowAnchor: [12, 38],
+			popupAnchor: [0, -30]
+		}
+	});
+	var redIcon = new tinyIcon({ iconUrl: '../assets/marker-red.png' });
+	var yellowIcon = new tinyIcon({ iconUrl: '../assets/marker-yellow.png' });
+
+	var sentData = {};
+
+	var connects = {};
+	var markers = {};
+	var active = false;
+
+	socket.on('load:coords', function(data) {
+		if (!(data.id in connects)) {
+			setMarker(data);
+			$("html, body").animate({ scrollTop: 0 }, "fast");
+		}
+
+		connects[data.id] = data;
+		connects[data.id].updated = $.now();
+	});
+
+	socket.on('load:chat', function(data) {
+		setMarkerChat(data)
+		snd.play();
+	});
+
+
+	// check whether browser supports geolocation api
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(positionSuccess, positionError, { enableHighAccuracy: true });
+	} else {
+		$('.map').text('Your browser is out of fashion, there\'s no geolocation!');
+	}
+
+	function positionSuccess(position) {
+		var lat = position.coords.latitude;
+		var lng = position.coords.longitude;
+		var acr = position.coords.accuracy;
+
+		// mark user's position
+		var userMarker = L.marker([lat, lng], {
+			icon: redIcon
+		});
+		// uncomment for static debug
+		// userMarker = L.marker([51.45, 30.050], { icon: redIcon });
+
+		// load leaflet map
+		map = L.map('map');
+
+		L.tileLayer('https://{s}.tiles.mapbox.com/v3/examples.map-i87786ca/{z}/{x}/{y}.png', { minZoom: 5, maxZoom: 20, detectRetina: true }).addTo(map);
+
+		// set map bounds
+		map.fitWorld();
+		userMarker.addTo(map);
+		userMarker.bindPopup('<p>You are there! Your ID is ' + userId + '</p>').openPopup();
+
+		var emit = $.now();
+		// send coords on when user is active
+		doc.on('mousemove', function() {
+			active = true;
+
+			sentData = {
+				id: userId,
+				active: active,
+				coords: [{
+					lat: lat,
+					lng: lng,
+					acr: acr
+				}]
+			};
+
+			if ($.now() - emit > 30) {
+				socket.emit('send:coords', sentData);
+				emit = $.now();
+			}
+		});
+
+		chatinput.on("keypress", function (e){
+			if (e.which == 13){
+			
+				var chat = $(this).val();
+				active = true;
+				sentData = {
+					id: userId,
+					active: active,
+					chat: chat,
+					coords: [{
+						lat: lat,
+						lng: lng,
+						acr: acr
+					}]
+				};
+				if ($.now() - emit > 30) {
+					//alert($(this).val())	
+					socket.emit('send:chat', sentData);
+					emit = $.now();
+				}	
+				$(this).val("")
+				$("#chatoutput").append("<div class='me'>" + chat + "</div>");
+				$('#chatoutput').scrollTop($('#chatoutput')[0].scrollHeight);
+			}
+			
+		}); 
+
+	}
+
+	doc.bind('mouseup mouseleave', function() {
+		active = false;
+	});
+
+	// showing markers for connections
+	function setMarker(data) {
+		for (var i = 0; i < data.coords.length; i++) {
+			var marker = L.marker([data.coords[i].lat, data.coords[i].lng], { icon: yellowIcon }).addTo(map);
+			marker.bindPopup('<p>One more external user is here!</p>').openPopup();
+			markers[data.id] = marker;
+		}
+	}
+
+	// showing markers for connections
+	function setMarkerChat(data) {
+		$("#chatoutput").append("<div class='you'>" + data.id + " : " + data.chat + "</div>");
+		$('#chatoutput').scrollTop($('#chatoutput')[0].scrollHeight);
+		for (var i = 0; i < data.coords.length; i++) {
+			var marker = L.marker([data.coords[i].lat, data.coords[i].lng], { icon: yellowIcon }).addTo(map);
+			marker.bindPopup(data.chat).openPopup();
+			markers[data.id] = marker;
+		}
+	}
+
+	// handle geolocation api errors
+	function positionError(error) {
+		var errors = {
+			1: 'Authorization fails', // permission denied
+			2: 'Can\'t detect your location', //position unavailable
+			3: 'Connection timeout' // timeout
+		};
+		showError('Error:' + errors[error.code]);
+	}
+
+	function showError(msg) {
+		info.addClass('error').text(msg);
+
+		doc.click(function() {
+			info.removeClass('error');
+		});
+	}
+
+	setTimeout(function() {
+    	$('#header').fadeOut('slow');
+
+	}, 4000);
+
+	// delete inactive users every 15 sec
+	setInterval(function() {
+		for (var ident in connects){
+			if ($.now() - connects[ident].updated > 15000) {
+				delete connects[ident];
+				map.removeLayer(markers[ident]);
+			}
+		}
+	}, 15000);
+});
---
+++ server.js
@@ -0,0 +1,32 @@
+var http = require('http');
+var Static = require('node-static');
+var app = http.createServer(handler);
+var io = require('socket.io').listen(app);
+var port = 8080;
+
+var files = new Static.Server('./public');
+
+function handler (request, response) {
+	request.on('end', function() {
+		files.serve(request, response);
+	}).resume();
+}
+
+// delete to see more logs from sockets
+io.set('log level', 1);
+
+io.sockets.on('connection', function (socket) {
+	console.log(socket.conn.transport.name);
+	
+	socket.on('send:coords', function (data) { 
+		socket.broadcast.emit('load:coords', data);		
+	});
+
+	socket.on('send:chat', function (data) { 
+		socket.broadcast.emit('load:chat', data);		
+	});
+});
+
+// start app on specified port
+app.listen(port);
+console.log('Your server goes on localhost:' + port);
